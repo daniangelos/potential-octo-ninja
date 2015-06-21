0f1cb93e747f48acf334618aef15be89f9044a12
Daniella Angelos <daniangelos@hotmail.com>
2015-06-20 21:28:38 -0300

    server arrumado do merge

--- src/server.py
+++ src/server.py
@@ -187,6 +187,7 @@ def int_to_bit(n):
 def bit32_to_int(bitfield):
     total = 0
     i = 0
+    print (bitfield)
     for number in reversed(bitfield):
         if(number == 1):
             total += 2**i
@@ -282,16 +283,13 @@ def receber_dados():
                     bitfield_msg = flipar_impares(bitfield_msg)
 
                 #Passa o bitfield para inteiro
-<<<<<<< HEAD
                 _mensagem = bitfield_msg[0:32]
                 _check = bitfield_msg[32:]
                 data_loaded['msg'] = bit32_to_int(_mensagem)
                 data_loaded['check'] = bit32_to_int(_check)
-=======
                 data_loaded['msg']
                 data_loaded['msg'] = bit32_to_int(bitfield_msg)
                 data_loaded['check'] = bit32_to_string(vetor_bits_check)
->>>>>>> 2800b676547c243cb5e9201fb935b48c26bb853a
                 # Coloca o dado recebido em uma fila sincrona de dados para serem enviados
                 to_send.put(data_loaded)
                 # FIM receber_dados
